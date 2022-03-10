f0d70f54656f9d6a23b39c6cde4f04c49854d483
Andrew Komlyev <andrew.komlyev@gmail.com>
2022-03-10 09:44:37 -0800

    Grab Object

--- VGP233_Final_Project/Assets/Scripts/GrabObject.cs
+++ VGP233_Final_Project/Assets/Scripts/GrabObject.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GrabObject : MonoBehaviour
 {
+
+    RaycastHit hit;
+    GameObject grabbedObject;
+    public Transform grabPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,34 @@ public class GrabObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E) && Physics.Raycast(transform.position, transform.forward, out hit, 5) && hit.transform.GetComponent<Rigidbody>())
+        {
+            grabbedObject = hit.transform.gameObject;
+
+        }
+        else if(Input.GetKeyUp(KeyCode.E))
+        {
+            grabbedObject = null;
+
+
+        }
+
+        if(grabbedObject)
+        {
+
+            grabbedObject.GetComponent<Rigidbody>().velocity =10 * (grabPosition.position- grabbedObject.transform.position);
+            grabbedObject.transform.rotation = Quaternion.Euler(grabbedObject.transform.rotation.x, grabbedObject.transform.rotation.y, grabbedObject.transform.rotation.z);
+            
+
+
+
+        }
+        //while (grabbedObject)
+        //{
+        //    grabbedObject.GetComponent<Rigidbody>().mass = 0;
+
+        //}
+
+
     }
 }
